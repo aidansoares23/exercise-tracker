164fc472647a95d45bdc0c6a93eb792d5c840fc8
Aidan Soares <aidansoares@gmail.com>
2025-12-17 17:25:43 -0800

    point backend at hosted frontend

--- soaresa_rest/exercises_controller.mjs
+++ soaresa_rest/exercises_controller.mjs
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://exercise-tracker-backend-ldcr.onrender.com"
+    "https://exercise-tracker-dusky-gamma.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
